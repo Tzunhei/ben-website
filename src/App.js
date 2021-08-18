87c07823f1015fe5ab9ced9e2ccf2031fe159b88
Clément Yam <yamtzunhei@gmail.com>
2021-08-18 16:22:37 +0200

    build home page

--- src/App.js
+++ src/App.js
@@ -1,24 +1,25 @@
-import logo from './logo.svg';
-import './App.css';
+import { CssBaseline, ThemeProvider, createTheme } from '@material-ui/core';
+import Home from './pages/Home';
+
+const theme = createTheme({
+  typography: {
+    h3: {
+      fontFamily: 'Atkinson Hyperlegible',
+      fontStyle: 'italic',
+    },
+    h5: {
+      fontFamily: 'Atkinson Hyperlegible',
+      fontStyle: 'italic',
+    },
+  },
+});
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <ThemeProvider theme={theme}>
+      <CssBaseline />
+      <Home />
+    </ThemeProvider>
   );
 }
 
